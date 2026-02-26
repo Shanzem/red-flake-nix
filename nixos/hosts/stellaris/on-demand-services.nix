18d6d0cc20054ef0f0ea010f2e57b717dce3febf
Mag1cByt3s <ppeinecke@protonmail.com>
2026-02-26 06:37:12 +0100

    nixos: make docker truly on-demand
    Disable docker.socket wantedBy so socket activation doesn't start Docker on boot, and stop overriding containerd.service (it created an invalid unit).

--- nixos/hosts/stellaris/on-demand-services.nix
+++ nixos/hosts/stellaris/on-demand-services.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   # Keep these services installed/configured, but don't start them automatically.
   # Start them when needed via `sudo systemctl start <name>`.
@@ -7,9 +7,16 @@
     #neo4j.wantedBy = lib.mkForce [ ];
     #postgresql.wantedBy = lib.mkForce [ ];
 
-    docker.wantedBy = lib.mkForce [ ];
-    containerd.wantedBy = lib.mkForce [ ];
+    docker = lib.mkIf config.virtualisation.docker.enable {
+      wantedBy = lib.mkForce [ ];
+    };
 
     ollama.wantedBy = lib.mkForce [ ];
+  };
+
+  # Docker is socket-activated by default on NixOS. If you want it truly "manual",
+  # disable the socket too, otherwise any access to /run/docker.sock will start it.
+  systemd.sockets.docker = lib.mkIf config.virtualisation.docker.enable {
+    wantedBy = lib.mkForce [ ];
   };
 }
