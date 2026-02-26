734a9d06412c83309a0dc48710242524de384445
Mag1cByt3s <ppeinecke@protonmail.com>
2026-02-26 07:11:05 +0100

    stellaris: disable some bootup services

--- nixos/hosts/stellaris/on-demand-services.nix
+++ nixos/hosts/stellaris/on-demand-services.nix
@@ -7,11 +7,21 @@
     #neo4j.wantedBy = lib.mkForce [ ];
     #postgresql.wantedBy = lib.mkForce [ ];
 
+    # do not start docker on bootup
     docker = lib.mkIf config.virtualisation.docker.enable {
       wantedBy = lib.mkForce [ ];
     };
 
+    # do not start podman on bootup
+    podman = lib.mkIf config.virtualisation.podman.enable {
+      wantedBy = lib.mkForce [ ];
+    };
+
+    # do not start ollama on bootup
     ollama.wantedBy = lib.mkForce [ ];
+
+    # do not start usbmuxd on bootup
+    usbmuxd.wantedBy = lib.mkForce [ ];
   };
 
   # Docker is socket-activated by default on NixOS. If you want it truly "manual",
@@ -19,4 +29,19 @@
   systemd.sockets.docker = lib.mkIf config.virtualisation.docker.enable {
     wantedBy = lib.mkForce [ ];
   };
+
+  # Podman is socket-activated by default on NixOS. If you want it truly "manual",
+  # disable the socket too, otherwise any access to /run/podman/podman.sock will start it.
+  systemd.sockets.podman = lib.mkIf config.virtualisation.podman.enable {
+    wantedBy = lib.mkForce [ ];
+  };
+
+  # Disable default fwupd-refresh.timer. We do this manually
+  systemd.timers.fwupd-refresh.wantedBy = lib.mkForce [ ];
+
+  # Disable default nh-clean.timer. We do this manually
+  systemd.timers.nh-clean.wantedBy = lib.mkForce [ ];
+
+  # Disable default podman-prune.timer. We do this manually
+  systemd.timers.podman-prune.wantedBy = lib.mkForce [ ];
 }
